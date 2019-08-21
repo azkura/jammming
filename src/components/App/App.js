1c35a86841456d7cb8bd428ca27938044d93634e
github_username <azizkoura@hotmail.com>
2019-08-21 15:47:59 +0200

    create Playlist component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import SearchBar from '../SearchBar/SearchBar'
 import SearchResults from '../SearchResults/SearchResults'
+import Playlist from '../Playlist/Playlist'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <SearchBar />
         <div className="App-playlist">
           <SearchResults />
+          <Playlist />
         </div>
       </div>
     </div>
