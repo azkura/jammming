baa2b91b69a4019430858e038ed6764f27090c6f
github_username <azizkoura@hotmail.com>
2019-08-21 15:25:15 +0200

    creating searchBar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import SearchBar from '../SearchBar/SearchBar'
+
 function App() {
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
+        <SearchBar />
         <div className="App-playlist">
         </div>
       </div>
