1be0ca77f81dc950443d173585d3525863d1b3da
github_username <azizkoura@hotmail.com>
2019-08-21 18:40:25 +0200

    passin tracks throught playlist component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,13 @@ class App extends React.Component {
         {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
         {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
         {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
-      ] 
+      ],
+      playlistName: 'pop',
+      playlistTracks: [
+        {name: 'i like that', artist: 'houston', album: 'like', id: 4},
+        {name: 'bee', artist: 'bop', album: 'beep', id: 5},
+        {name: 'that', artist: 'big', album: 'that', id: 6}
+      ]
     }
   }
   render() {
@@ -24,7 +30,9 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <Playlist 
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
