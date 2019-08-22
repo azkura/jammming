2e56630f7c6a9185503f49b1eae078224837510e
github_username <azizkoura@hotmail.com>
2019-08-22 13:06:15 +0200

    Hook up Search Bar to Spotify Search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,17 +6,22 @@ class SearchBar extends React.Component {
     super(props)
 
     this.search = this.search.bind(this)
+    this.handleTermChange = this.handleTermChange.bind(this)
   }
 
   search() {
-    this.props.onSearch(this.props.search)
+    this.props.onSearch(this.state.term)
+  }
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value})
   }
 
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <button className="SearchButton" onClick={this.search}>SEARCH</button>
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
+        <button className="SearchButton">SEARCH</button>
       </div>
     )
   }
